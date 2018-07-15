05f83561d298b7e56272f2948728f1855d613cf7
Ben Dolly <bromeodolly@gmail.com>
2018-07-15 15:06:36 -0400

    udpates package.json

---
+++ src/utils/styling.js
@@ -0,0 +1,50 @@
+import styled from 'react-emotion';
+import {
+  space,
+  width,
+  fontSize,
+  color,
+  lineHeight,
+  fontWeight,
+  borders,
+  borderRadius,
+  alignItems,
+  justifyContent,
+  flexGrow,
+  flexWrap,
+  flexBasis,
+  flexDirection,
+  justifySelf,
+  alignSelf,
+  maxWidth,
+  complexStyle,
+  hover,
+  textAlign
+} from 'styled-system';
+
+export const applyProp = (name, value) => (value ? `${name}: ${value};` : ``);
+export const overflow = ({ overflow }) => applyProp(`overflow`, overflow);
+export const overflowY = ({ overflowY }) => applyProp(`overflow-y`, overflowY);
+const baseStyles = complexStyle({
+  prop: 'baseStyle',
+  key: 'baseStyles'
+});
+
+export const applyDefaultStyles = Component => styled(Component)`
+  ${baseStyles}
+  ${space}
+  ${width}
+  ${fontSize}
+  ${fontWeight}
+  ${color}
+  ${maxWidth}
+  ${justifySelf}
+  ${alignSelf}
+  ${borders}
+  ${borderRadius}
+  ${lineHeight}
+  ${overflow}
+  ${overflowY}
+  ${hover}
+  ${textAlign}
+`;
