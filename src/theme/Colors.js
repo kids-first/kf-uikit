0bfb615d2446a74d7538e61f299f93b1ce997d03
Dan Kolbman <dankolbman@gmail.com>
2018-07-19 09:33:29 -0400

    :recycle: Make colors programmatic

--- src/theme/Colors.js
+++ src/theme/Colors.js
@@ -1,39 +1,37 @@
-const colors = {
+import chroma from 'chroma-js';
+
+const pallette = {
+  // B/W
+  white: 'rgb(255,255,255)',
+  black: 'rgb(0,0,0)',
+  grey: [...Array(12).keys()].map(i => chroma('rgb(0,0,0)').brighten(i/2)),
+
+  // Branding
   primary: 'rgb(144,39,142)',   //magenta
   secondary: 'rgb(43,56,143)',  //purplish blue
   tertiary: 'rgb(0,155,184)',   //teal-blue
 
-  white: 'rgb(255,255,255)',
-  black: 'rgb(0,0,0)',
+  info: 'rgb(232,58,156)', // pink
+  error: 'rgb(232,58,156)',
+}
 
-  highlight: 'rgb(232,58,156)', //pink
+const colors = {
+  ...pallette,
 
   active: 'rgb(0,175,237)',     //light blue
-  inactive: 'rgb(222,223,228)', //grey
+  inactive: '#999',
 
   optionSelected: 'rgb(229,246,253)', //light blue
-  grey: [
-    'rgb(237,238,241)',         //grey for table backgrounds
-    'rgb(116,117,125)',         //rgb(116, 117, 125) dark grey text on greyScale5
-    'rgb(212,214,221)',
-    'rgb(107,98,98)',
-    'rgb(245,245,245)',
-    'rgb(224,225,230)',
-    'rgb(212, 214, 221)',       //#d4d6dd
-    'rgb(144,144,144)',         //not enough contrast on white background
-    'rgb(61,61,61)',
-    'rgb(52, 52, 52)',          //#343434
-    'rgb(36,36,36)'
-  ],
 
   hover: {
-    main: 'rgb(192,50,153)', //also pink
-    primary: 'rgb(64,76,154)', //purple
-    tertiary: 'rgb(25,169,196)' //lighter teal-blue
+    main: chroma(pallette.info).brighten(0.1),
+    primary: chroma(pallette.primary).brighten(1),
+    tertiary: chroma(pallette.secondary).brighten(1)
   },
+
   background: {
-    grey: 'rgb(244,245,248)',
-    error: 'rgb(249,222,225)',
+    grey: pallette.grey[1],
+    error: pallette.error,
     tertiary: 'rgb(237,238,241)' // light light blue
   },
   border: {
