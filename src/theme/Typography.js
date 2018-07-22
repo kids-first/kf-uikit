e94c1e515f5992155763a0c6641004f96efcbbc2
Dan Kolbman <dankolbman@gmail.com>
2018-07-21 20:35:21 -0400

    :sparkles: Add storybook build command

--- src/theme/Typography.js
+++ src/theme/Typography.js
@@ -1,5 +1,8 @@
-const fontSizes = [
+export const fontSizes = [
   12, 14, 16, 24, 32, 48, 64, 96, 128
 ]
 
-export default fontSizes;
+export const fonts = {
+  default: 'Montserrat, Helvetica, sans-serif',
+  details: 'Open Sans'
+};
