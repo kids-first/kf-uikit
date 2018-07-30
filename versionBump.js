6dffd08ddcb65b211453d99d4a6bd141130b6d61
Ben Dolly <bromeodolly@gmail.com>
2018-07-30 10:24:18 -0400

    adds versionBump to `npm run storybook:build`
    adds current package.json version number to to versions storybook config and updates the netflify redirect to point to the most current directory

---
+++ versionBump.js
@@ -0,0 +1,28 @@
+const replace = require("replace-in-file");
+const pjVersion = require("./package.json").version;
+const fs = require("fs");
+
+const options = [
+  {
+    files: "storybook-config.json",
+    from: '"]',
+    to: '","' + pjVersion + '"]'
+  },
+  {
+    files: "_redirects",
+    from: /[0-9].*/,
+    to: pjVersion
+  }
+];
+
+options.map(_opt => {
+  try {
+    const changes = replace.sync(_opt);
+    console.log("Modified files:", changes.join(", "));
+    fs
+      .createReadStream("./" + _opt.files)
+      .pipe(fs.createWriteStream("./dist/" + _opt.files));
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+});
