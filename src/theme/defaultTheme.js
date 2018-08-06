5a17b4798030f5a131b0066f577c2e4b99a1260b
Dan Kolbman <dankolbman@gmail.com>
2018-08-06 14:17:46 -0400

    :rotating_light: Fix linting errors in default theme

--- src/theme/defaultTheme.js
+++ src/theme/defaultTheme.js
@@ -1,39 +1,12 @@
-import { css } from '../kfFeels/kfEmotion';
-
 import {
   headings,
   textUtils,
   fonts,
   paragraph,
   blockquote,
-  lists
+  lists,
 } from './Typography';
 import colors from './Colors';
-
-// import buttons from './Buttons';
-// import layout from './Layout';
-
-import layout from './Layout';
-
-const components = {
-  textStyles: {
-    ...textUtils
-  },
-
-  dropdownContainer: css`
-    position: absolute;
-    top: 100%;
-    backgroundcolor: white;
-    border: 1px solid ${colors.greyScale4};
-    width: 100%;
-    font-size: 14px;
-  `,
-
-  //no styles for these yet
-  error: css``,
-  warning: css``,
-  info: css``
-};
 
 export default {
   colors,
@@ -43,6 +16,6 @@ export default {
   blockquote,
   ...lists,
   textStyles: {
-    ...textUtils
-  }
+    ...textUtils,
+  },
 };
