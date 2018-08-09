36592958564fa03ce2a35522f3e6ee9aabea7067
Ben Dolly <bromeodolly@gmail.com>
2018-08-09 11:21:22 -0400

    adds info addon to TypeSpecimen story

--- stories/01. Typography/index.jsx
+++ stories/01. Typography/index.jsx
@@ -70,76 +70,80 @@ stories.add(
   ),
 );
 
-stories.add('Type Specimen', () => (
-  <div>
-    <H1>Heading H1</H1>
-    <H2>Heading H2</H2>
-    <H3>Heading H3</H3>
-    <H4>Heading H4</H4>
-    <H5>Heading H5</H5>
-    <Paragraph>
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-      invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
-      justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-      ipsum dolor sit amet.
-    </Paragraph>
-    <hr />
-    <H2 mb="0">Main Headline Goes Here</H2>
-    <Paragraph>
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-      invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
-      justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-      ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos
-      et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-      sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
-      <TextList.unordered>
-        <li>Adolescent Idiopathic Scoliosis </li>
-        <li>Cancer Susceptibility </li>
-        <li>Congenital Diaphragmatic Hernia </li>
-        <li>Craniofacial Microsomia </li>
-        <li>Disorders of Sex Development </li>
-        <li>Enchondromatoses </li>
-        <li>Ewing Sarcoma </li>
-        <li>Familial Leukemia </li>
-        <li>Hearing Loss </li>
-      </TextList.unordered>
-      consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-      aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-    </Paragraph>
-    <BlockQuote>
-      Contingent on available funds, the DRC award is expected to provide funding for five years of
-      up to a total of approximately $14.8 million.
-    </BlockQuote>
-    <hr />
-    <H4>Users can get started in the Kids First DRC Data Portal in three easy steps:</H4>
-    <Paragraph>
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-      invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam et
-      justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-      ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos
-      et accusam et justo duo dolores et ea rebum.
-      <TextList.ordered>
-        <li>Register for the Kids First DRC Data Portal and complete your online profile.</li>
-        <li>
-          Browse and search through clinical &amp; phenotypic data in the File Repository to build a
-          cohort of interest.
-        </li>
-        <li>
-          Push your dbGaP authorized genomic files to Cavatica (or download) to begin your analysis
-          on the data.
-        </li>
-      </TextList.ordered>
-      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
-      dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-      et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-      et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-      amet.
-    </Paragraph>
-  </div>
-));
+stories.add(
+  'Type Specimen',
+  withInfo({ inline: false }, () => (
+    <div>
+      <H1>Heading H1</H1>
+      <H2>Heading H2</H2>
+      <H3>Heading H3</H3>
+      <H4>Heading H4</H4>
+      <H5>Heading H5</H5>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+        et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+        Lorem ipsum dolor sit amet.
+      </Paragraph>
+      <hr />
+      <H2 mb="0">Main Headline Goes Here</H2>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+        et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+        diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+        gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+        amet.
+        <TextList.unordered>
+          <li>Adolescent Idiopathic Scoliosis </li>
+          <li>Cancer Susceptibility </li>
+          <li>Congenital Diaphragmatic Hernia </li>
+          <li>Craniofacial Microsomia </li>
+          <li>Disorders of Sex Development </li>
+          <li>Enchondromatoses </li>
+          <li>Ewing Sarcoma </li>
+          <li>Familial Leukemia </li>
+          <li>Hearing Loss </li>
+        </TextList.unordered>
+        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+        magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      </Paragraph>
+      <BlockQuote>
+        Contingent on available funds, the DRC award is expected to provide funding for five years
+        of up to a total of approximately $14.8 million.
+      </BlockQuote>
+      <hr />
+      <H4>Users can get started in the Kids First DRC Data Portal in three easy steps:</H4>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam
+        et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+        diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+        <TextList.ordered>
+          <li>Register for the Kids First DRC Data Portal and complete your online profile.</li>
+          <li>
+            Browse and search through clinical &amp; phenotypic data in the File Repository to build
+            a cohort of interest.
+          </li>
+          <li>
+            Push your dbGaP authorized genomic files to Cavatica (or download) to begin your
+            analysis on the data.
+          </li>
+        </TextList.ordered>
+        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+        ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
+        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+        dolor sit amet.
+      </Paragraph>
+    </div>
+  )),
+);
 
 stories.add('H1', withInfo({ text: theme.h1 }, () => <H1>{text('H1 text', 'Heading H1')}</H1>));
 stories.add('H2', withInfo({ text: theme.h2 }, () => <H2>{text('H2 text', 'Heading H2')}</H2>));
