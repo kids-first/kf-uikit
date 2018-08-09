d870e36d1610a848dc6a15afac33fec34eb0e51d
Ben Dolly <bromeodolly@gmail.com>
2018-08-09 12:53:26 -0400

    :rotating_light: making eslint happy for kfEmotion

--- src/kfEmotion/index.js
+++ src/kfEmotion/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import createEmotionStyled from 'create-emotion-styled';
 import * as emotion from './kfEmotion';
+
 export * from './kfEmotion';
 export const styled = createEmotionStyled(emotion, React);
-
-
