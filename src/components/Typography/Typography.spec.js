e7b3b132969dd5ef03f9fe699df730a10bc1b494
Ben Dolly <bromeodolly@gmail.com>
2018-08-11 23:52:39 -0400

    :construction: dedupe class declarations and write to file

--- src/components/Typography/Typography.spec.js
+++ src/components/Typography/Typography.spec.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { createSerializer } from 'jest-emotion';
 import * as emotion from '../../kfFeels/kfEmotion';
-import { H1, H2 } from '.';
+import { H1, H2, H3 } from '.';
 
 test('H1 renders with correct styles', () => {
   const actual = create(<H1>hello world</H1>);
@@ -11,5 +11,14 @@ test('H1 renders with correct styles', () => {
 
 test('H2 renders with correct styles', () => {
   const actual = create(<H2>hello world</H2>);
+  expect(actual).toMatchSnapshot();
+});
+
+test('H3 renders with correct styles', () => {
+  const actual = create(
+    <H3>
+      hello world <H1>foobar</H1>
+    </H3>,
+  );
   expect(actual).toMatchSnapshot();
 });
