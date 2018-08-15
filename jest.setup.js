1bbbd94ce58a2c532eea41f1ed419ab347b0c480
Ben Dolly <bromeodolly@gmail.com>
2018-08-15 14:52:08 -0400

    :fire: src/utils/kf-jest-serializer port
    refactored into `./build`

--- jest.setup.js
+++ jest.setup.js
@@ -1,25 +1,15 @@
 /* eslint-disable */
 import React from 'react';
-import * as css from 'css';
-import cssscss from 'css-scss';
-import { writeFileSync, readFile, readFileSync } from 'fs';
-import { invert, mapValues } from 'lodash';
-import { promisify } from 'util';
+import { createSerializer } from 'jest-emotion';
 import { renderToStaticMarkup } from 'react-dom/server';
 import 'jest-enzyme';
-import Enzyme, { shallow, render, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { createSerializer, extractThemeVars } from './src/utils/kf-jest-serializer/src';
-import {
-  replaceWithCssVars,
-  writeStaticFile,
-} from './src/utils/kf-jest-serializer/src/extract-static';
+import { shallow, render, mount } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
 // import { axe, toHaveNoViolations } from 'jest-axe';
 import { create } from 'react-test-renderer';
 import * as emotion from './src/kfFeels/kfEmotion';
 import KFThemeProvider from './src/index';
-import defaultTheme from './src/theme/defaultTheme';
-const fsWriteFile = promisify(readFile);
+
 const renderWithTheme = renderFn => (component, ...rest) =>
   renderFn(<KFThemeProvider>{component}</KFThemeProvider>, rest);
 
@@ -46,16 +36,6 @@ global.render = renderWithTheme(render);
 global.create = renderWithTheme(create);
 global.mount = mountWithTheme;
 global.renderToHtml = renderWithTheme(renderToStaticMarkup);
-
-global.cssVarsMap = {};
-global.cssVarsRoot = '';
-global.cssAST = JSON.parse(readFileSync(process.cwd() + '/src/css/AST.json', 'utf-8'));
-global.filePaths = {
-  css: process.cwd() + '/src/css/kf-uikit.css',
-  cssStatic: process.cwd() + '/src/css/kf-uikit-static.min.css',
-  scss: process.cwd() + '/src/css/kf-uikit.scss',
-  cssAST: process.cwd() + '/src/css/AST.json',
-};
 // global.axe = axe;
 
 // This is defined by webpack in storybook builds using the DefinePlugin plugin.
@@ -73,38 +53,3 @@ expect.addSnapshotSerializer(
     },
   }),
 );
-
-global.beforeAll(() => {
-  console.log('beforeAll');
-  global.cssVarsRoot = extractThemeVars(defaultTheme);
-  // clear files
-  // Object.values(global.filePaths).forEach(path => fsWriteFile(path, ''));
-});
-
-global.afterAll(() => {
-  console.log('afterAll');
-
-  // static minified css. no vars
-  writeStaticFile(global.filePaths.cssStatic, css.stringify(global.cssAST))
-    .then(data => {
-      console.log(`WROTE ${data.file}`);
-      return data.data;
-    })
-    .then(cssStaticData => {
-      const ASTwithCSSvars = replaceWithCssVars(css.parse(cssStaticData), global.cssVarsMap);
-      // css with vars
-      writeFileSync(
-        global.filePaths.css,
-        `:root{${global.cssVarsRoot}} \n\n ${css.stringify(ASTwithCSSvars)}`,
-      );
-      console.log(`WROTE ${global.filePaths.css}`);
-      return css.stringify(ASTwithCSSvars);
-    })
-    .then(cssWvars => {
-      const cssFile = readFileSync(global.filePaths.css, 'utf-8');
-      // scss from css vars
-      writeFileSync(global.filePaths.scss, cssscss(cssFile));
-      console.log(`WROTE ${global.filePaths.scss}`);
-    })
-    .catch(err => console.log(err));
-});
