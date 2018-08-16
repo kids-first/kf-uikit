110078c17fc1bb487497b65f736dbaa7d6a17e4c
Dan Kolbman <dankolbman@gmail.com>
2018-08-16 16:45:37 -0400

    :art: Rewrite with tailwinds

--- stories/Colors/Colors.jsx
+++ stories/Colors/Colors.jsx
@@ -1,114 +1,30 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import { space, color, fontSize } from 'styled-system';
-import chroma from 'chroma-js';
-import styled from '../../src/kfFeels/kfReactEmotion';
-import { H3 } from '../../src/components/Typography';
-import { Paragraph } from '../../src/components/Typography/Text';
-import { Box } from '../../src/components/Layout';
-import { wcagGrade } from '../../src/utils/colors';
 
 /**
  * A basic square swatch to display a color
  */
-/* eslint-disable no-shadow */
-const Swatch = styled('div')`
-  width: 100px;
-  height: 100px;
-  display: block;
-  border-radius: 4px;
-  margin: auto;
-  background-color: ${({ color }) => color};
-  ${space} ${color};
-`;
+function Swatch(props) {
+  const { color, children } = props;
+  return (
+    <div className="container w-32 m-3 text-center">
+      <div className={`bg-${color} w-32 h-32`} />
+      <p>
+        <h3>{color}</h3>
+        {children}
+      </p>
+    </div>
+  );
+}
 
 Swatch.propTypes = {
-  ...space.propTypes,
-  ...color.propTypes,
-};
-
-/**
- * A basic square swatch to display a gradient
- */
-const GradientSwatch = styled(Swatch)`
-  width: 256px;
-  background-color: none;
-  background-image: ${({ gradient }) => gradient};
-`;
-/**
- * A simple badge
- */
-const Badge = styled('span')`
-  border-radius: 4px;
-  border-width: 1px;
-  border-color: '#333';
-  padding: 3px;
-  margin: 5px;
-  display: block;
-  ${color} ${fontSize};
-`;
-Badge.propTypes = {
-  ...space.propTypes,
-  ...color.propTypes,
-};
-
-/**
- * Displays a color swatch with its hex and rgba values along with a type
- * specimen on a badge of that color
- */
-
-const ColorSpecimen = (
-  { color, name }, // eslint-disable-line no-shadow
-) => (
-  <Box m={24} w={256} display="inline-block">
-    <Swatch color={chroma(color).css()} />
-    <H3 mb={1}>{name}</H3>
-    <Paragraph mt={0} mb={0} small>
-      hex: {chroma(color).hex()}
-    </Paragraph>
-    <Paragraph mt={0} small>
-      rgb: {chroma(color).css()}
-    </Paragraph>
-    <Badge color="black" bg={chroma(color).css()}>
-      {wcagGrade('black', color)}
-    </Badge>
-    <Badge color="white" bg={chroma(color).css()}>
-      {wcagGrade('white', color)}
-    </Badge>
-  </Box>
-);
-
-ColorSpecimen.propTypes = {
-  name: propTypes.string,
   color: propTypes.string,
+  children: propTypes.element,
 };
 
-ColorSpecimen.defaultProps = {
-  name: 'Color',
-  color: '#999',
+Swatch.defaultProps = {
+  color: 'primary',
+  children: null,
 };
 
-/**
- * Displays a gradient swatch with title and value
- */
-export const GradientSpecimen = ({ gradient, name }) => (
-  <Box m={24} w={256} display="inline-block">
-    <GradientSwatch gradient={gradient} />
-    <H3 mb={1}>{name}</H3>
-    <Paragraph mt={0} mb={0} small>
-      hex: {gradient}
-    </Paragraph>
-  </Box>
-);
-
-GradientSpecimen.propTypes = {
-  name: propTypes.string,
-  gradient: propTypes.string,
-};
-
-GradientSpecimen.defaultProps = {
-  name: 'Gradient',
-  gradient: 'linear-gradient(to right, #fff, #000)',
-};
-
-export default ColorSpecimen;
+export default Swatch;
--- stories/Colors/Colors.story.jsx
+++ stories/Colors/Colors.story.jsx
@@ -1,136 +1,34 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { pick } from 'lodash';
-import colors from '../../src/theme/Colors';
-import { H2 } from '../../src/components/Typography/Typography';
-import { Box } from '../../src/components/Layout';
-import { hashColors, processColor } from '../../src/utils/colors';
-import ColorSpecimen, { GradientSpecimen } from './Colors';
+import Swatch from './Colors';
+import '../../src/css/tailwind.css';
 
-const uikitStories = storiesOf('Colors', module);
+const stories = storiesOf('Colors', module);
 
-const brandColors = hashColors(pick(colors, ['primary', 'secondary', 'tertiary', 'white']));
-
-uikitStories.add('Brand', () => (
-  <div
-    style={{
-      display: 'flex',
-      flexWrap: 'wrap',
-      flexDirection: 'row',
-    }}
-  >
-    {Object.keys(brandColors).map(name => (
-      <ColorSpecimen key={name} color={brandColors[name].hex} name={name} />
-    ))}
+const brand = ['primary', 'secondary', 'tertiary'];
+const brandSwatches = brand.map(color => <Swatch color={color} />);
+stories.add('Brand', () => (
+  <div className="flex">
+    {brandSwatches}
   </div>
 ));
 
-const greyScaleHash = colors.grey
-  .map((hex, idx) => ({ [`grey${idx}`]: hex }))
-  .reduce((acc, grey, idx) => {
-    return { ...acc, [Object.keys(grey)[0]]: processColor(colors.grey[idx]) };
-  }, {});
 
-uikitStories.add('GreyScale', () => (
-  <div
-    style={{
-      display: 'flex',
-      flexWrap: 'wrap',
-      flexDirection: 'row',
-    }}
-  >
-    {Object.keys(greyScaleHash).map(name => (
-      <ColorSpecimen key={name} color={greyScaleHash[name].hex} name={name} />
-    ))}
+const state = ['info', 'warn', 'highlight'];
+const stateSwatches = state.map(color => <Swatch color={color} />);
+stories.add('State', () => (
+  <div className="flex">
+    {stateSwatches}
   </div>
 ));
 
-const stateColors = hashColors(
-  pick(colors, ['highlight', 'active', 'inactive', 'optionSelected', 'hover', 'error']),
-);
 
-uikitStories.add('States', () => (
-  <div
-    style={{
-      display: 'flex',
-      flexWrap: 'wrap',
-      flexDirection: 'row',
-    }}
-  >
-    {Object.keys(stateColors).map(name => {
-      if (stateColors[name].hex) {
-        return <ColorSpecimen key={name} color={stateColors[name].hex} name={name} />;
-      }
-      const nestedColors = stateColors[name];
-      return (
-        <Box p={1} mb={4} width="100%" border="1px solid">
-          <H2 width="100%">{name}</H2>
-          {Object.keys(nestedColors).map(nestedName => (
-            <ColorSpecimen
-              key={nestedName}
-              color={stateColors[name][nestedName].hex}
-              rgba={stateColors[name][nestedName].rgba}
-              name={nestedName}
-            />
-          ))}
-        </Box>
-      );
-    })}
-  </div>
-));
+const greys = ['black', 'grey-darkest', 'grey-darker', 'grey-dark',
+  'grey', 'grey-light', 'grey-lighter', 'grey-lightest', 'white'];
+const greySwatches = greys.map(color => <Swatch color={color} />);
 
-const bgColorsHash = hashColors(colors.background);
-uikitStories.add('Background', () => (
-  <div
-    style={{
-      display: 'flex',
-      flexWrap: 'wrap',
-      flexDirection: 'row',
-    }}
-  >
-    {Object.keys(bgColorsHash).map(name => (
-      <ColorSpecimen
-        key={name}
-        color={bgColorsHash[name].hex}
-        rgba={bgColorsHash[name].rgba}
-        name={name}
-      />
-    ))}
-  </div>
-));
-
-const borderColorsHash = hashColors(colors.border);
-uikitStories.add('Borders', () => (
-  <div
-    style={{
-      display: 'flex',
-      flexWrap: 'wrap',
-      flexDirection: 'row',
-    }}
-  >
-    {Object.keys(borderColorsHash).map(name => (
-      <ColorSpecimen
-        key={name}
-        color={borderColorsHash[name].hex}
-        rgba={borderColorsHash[name].rgba}
-        name={name}
-      />
-    ))}
-  </div>
-));
-
-uikitStories.add('Gradients', () => (
-  <div
-    style={{
-      display: 'flex',
-      flexWrap: 'wrap',
-      flexDirection: 'row',
-    }}
-  >
-    {Object.keys(colors.gradient).map(name => (
-      <div>
-        <GradientSpecimen key={name} gradient={colors.gradient[name]} name={name} />
-      </div>
-    ))}
+stories.add('Greyscale', () => (
+  <div className="flex">
+    {greySwatches}
   </div>
 ));
