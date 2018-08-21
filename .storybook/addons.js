39937cdf02caecdbea9a7003fa0509f86ede4143
Dan Kolbman <dankolbman@gmail.com>
2018-08-21 12:03:25 -0400

    :fire: Remove storybook versions

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,3 @@
-import '@buildit/storybook-addon-versions/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-a11y/register';
