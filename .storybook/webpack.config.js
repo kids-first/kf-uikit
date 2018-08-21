3032bd23bde8dccf2d9941250b2cccf2e68043e6
Dan Kolbman <dankolbman@gmail.com>
2018-08-21 12:02:08 -0400

    :construction_worker: Hot reloading for tailwind styles

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,15 +1,17 @@
-const path = require('path');
+const path = require('path')
 
-module.exports = (baseConfig, env, defaultConfig) => {
-  // Extend defaultConfig as you need.
-
-  // For example, add typescript loader:
-  defaultConfig.module.rules.push({
-    test: /\.(js|jsx)$/,
-    loaders: ['eslint-loader'],
-    exclude: [/node_modules/],
-    include: path.resolve(__dirname, '../'),
-  });
-
-  return defaultConfig;
-};
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, '../src/css/'),
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader'
+        ]
+      }
+    ]
+  }
+}
