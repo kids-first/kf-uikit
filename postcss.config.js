95d5689683856d0be19975d2e915a9ec09ff8533
Ben Dolly <bromeodolly@gmail.com>
2018-08-28 12:02:18 -0400

    :wrench: adds postcss-to-css-vars plugin to generate css vars from tailwind conif file

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-    plugins: [
-        require('tailwindcss')('./tailwind.js')
-    ]
-}
+  plugins: [
+    require('tailwindcss')('./tailwind.js'),
+    require('postcss-to-css-vars')({
+      theme: './tailwind.js',
+      exclude: ['options', 'modules', 'plugins'],
+      prefix: 'KF',
+    }),
+  ],
+};
