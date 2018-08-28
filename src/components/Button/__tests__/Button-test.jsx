8ce11df381b13057a0c3c8a82985d7c9a8ebd49f
Dan Kolbman <dankolbman@gmail.com>
2018-08-27 22:12:40 -0400

    :camera: Add snapshot testing

---
+++ src/components/Button/__tests__/Button-test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Button from '../Button';
+
+it('renders correctly', () => {
+    const tree = renderer.create(
+            <Button>Hello</Button>
+              ).toJSON();
+      expect(tree).toMatchSnapshot();
+});
+
+
