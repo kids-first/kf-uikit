a5c928088d5ef8711c2cf84ab659eea9a3a35d44
Dan Kolbman <dankolbman@gmail.com>
2018-09-01 17:28:05 -0400

    :truck: Move css import to storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
 import { checkA11y } from '@storybook/addon-a11y';
+import '../src/tailwind.src.css';
 
 addDecorator(checkA11y);
 
