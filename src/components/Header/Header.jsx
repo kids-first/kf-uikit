6586878fb225eed1d33e326b0439b9a849391b5c
Dan Kolbman <dankolbman@gmail.com>
2018-10-17 10:19:06 -0400

    :sparkles: Add onLogoClick handler for logo in nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,7 +13,8 @@ import './Header.css';
  * It contains common functionality for all of Kids-First that should remain
  * consistent, such as the user drop down and application navigation.
  */
-const Header = ({ buttons, children }) => {
+const Header = ({ buttons, logo, children }) => {
+  const { logoHref, onLogoClick } = logo;
 
   const navClass = className(
     'Header--navigation',
@@ -22,7 +23,9 @@ const Header = ({ buttons, children }) => {
   return (
     <header className='Header'>
       <nav className={navClass}>
-        <Logo />
+        <a href={logoHref} onClick={() => onLogoClick()}>
+          <Logo className='Header--logo' />
+        </a>
         <div className='Navigation--buttons'>
           {buttons ? buttons.map(button => button) : null}
         </div>
@@ -33,6 +36,12 @@ const Header = ({ buttons, children }) => {
 }
 
 Header.propTypes = {
+  logo: propTypes.shape({
+    /** Url for the Logo to redirect to */
+    logoHref: propTypes.string,
+    /** Action to perform when the logo is clicked */
+    onLogoClick: propTypes.func,
+  }),
   /** Buttons to display on the navbar */
   buttons: propTypes.node,
   /** Child elements to display on the navbar */
@@ -40,6 +49,10 @@ Header.propTypes = {
 };
 
 Header.defaultProps = {
+  logo: {
+    logoHref: '#',
+    onLogoClick: () => {},
+  },
   buttons: null,
   children: null,
 };
