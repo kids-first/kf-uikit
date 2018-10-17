dcb50bc7de07e62750856d265fbc8c8012df4269
Dan Kolbman <dankolbman@gmail.com>
2018-10-17 12:27:53 -0400

    :bug: Add Header and Card to exports

--- src/index.jsx
+++ src/index.jsx
@@ -3,3 +3,5 @@ import './tailwind.src.css';
 // Import components below
 export Button from './components/Button/Button';
 export Logo from './components/Logo/Logo';
+export Card from './components/Card/Card';
+export Header from './components/Header/Header';
