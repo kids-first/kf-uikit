328aeb5a6118f4a5429584178ffa5a3c71821f99
Dan Kolbman <dankolbman@gmail.com>
2018-12-10 17:36:50 -0500

    :bug: Export stats component

--- src/index.jsx
+++ src/index.jsx
@@ -6,3 +6,4 @@ export Logo from './components/Logo/Logo';
 export Card from './components/Card/Card';
 export Header from './components/Header/Header';
 export Icon from './components/Icon/Icon';
+export Stats from './components/Stats/Stats';
