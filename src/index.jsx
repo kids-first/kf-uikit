0cf64508bed9b8ee3e351628303bf34401ec6a08
Ben Dolly <bromeodolly@gmail.com>
2019-04-01 22:40:59 -0400

    :checkered_flag: Adds IE11 support for Grid .cell-N and .row-N classes
    uses postcss-autoprefixer to add IE11 support for all variations of the .cell and .row classes

--- src/index.jsx
+++ src/index.jsx
@@ -7,3 +7,4 @@ export Card from './components/Card/Card';
 export Header from './components/Header/Header';
 export Icon from './components/Icon/Icon';
 export Stats from './components/Stats/Stats';
+export GridContainer from './components/Grid/GridContainer';
