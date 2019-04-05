358c3169c5ec62160958763236b9520f50ea784f
Ben Dolly <bromeodolly@gmail.com>
2019-04-05 16:46:53 -0400

    :camera_flash: Adds GridContainer snapshot tests

--- src/components/Grid/GridContainer.jsx
+++ src/components/Grid/GridContainer.jsx
@@ -25,7 +25,7 @@ GridContainer.propTypes = {
   /** Any additional classes to be applied to the button */
   className: PropTypes.string,
   /** Children elements. */
-  children: PropTypes.string,
+  children: PropTypes.any,
   /** Collapse margins and gutters. */
   collapsed: PropTypes.oneOf([true, 'cells', 'rows']),
   /** wheter or not to apply margin: 0 auto; */
