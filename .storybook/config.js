a0b2bb15e858c9ef7d08f1b275d7a03d4f10ba36
Ben Dolly <bromeodolly@gmail.com>
2019-04-12 11:43:34 -0400

    updates storybook config to use percy

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { configure, addDecorator } from '@storybook/react';
+import { configure, getStorybook, setAddon, addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { checkA11y } from '@storybook/addon-a11y';
 import { withInfo } from '@storybook/addon-info';
 import centered from '@storybook/addon-centered';
+import createPercyAddon from '@percy-io/percy-storybook';
 import '../src/tailwind.src.css';
+
+const { percyAddon, serializeStories } = createPercyAddon();
+setAddon(percyAddon);
+
 
 addDecorator(
   withInfo({
@@ -16,13 +21,15 @@ addDecorator(withKnobs);
 addDecorator(centered);
 
 let req = {
-  colors: './stories/Colors/Colors.story.jsx',
-  typography: './stories/Typography/Typography.story.jsx',
-  buttons: './src/components/Button/Button.story.jsx',
   logo: './src/components/Logo/Logo.story.jsx',
+  typography: './stories/Typography/Typography.story.jsx',
+  colors: './stories/Colors/Colors.story.jsx',
+  // spacing: './stories/Spacing/Spacing.story.jsx',
+  // layout: './src/components/Grid/Grid.story.jsx',
+  icons: './src/components/Icon/Icon.story.jsx',
+  buttons: './src/components/Button/Button.story.jsx',
   cards: './src/components/Card/Card.story.jsx',
   header: './src/components/Header/Header.story.jsx',
-  icons: './src/components/Icon/Icon.story.jsx',
   stats: './src/components/Stats/Stats.story.jsx',
 };
 const storyReqs = require.context('../', true, /^.*\.story\.jsx$/);
@@ -32,3 +39,7 @@ function loadStories() {
 }
 
 configure(loadStories, module);
+
+serializeStories(getStorybook);
+
+
