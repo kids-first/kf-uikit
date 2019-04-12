908a4813967d68a40b8d6a23ac36759cf92eebde
Ben Dolly <bromeodolly@gmail.com>
2019-04-12 14:18:46 -0400

    :hankey: Percy setup for release v0.3.1
    :hankey: this pulls the commit tagged for release 0.3.1 and adds the percy setup to it so
    we can have a baseline percy snapshot against the latest release

--- src/components/Header/Header.story.jsx
+++ src/components/Header/Header.story.jsx
@@ -11,10 +11,10 @@ stories.addDecorator(withKnobs);
 
 stories.add('Header', () => (
   <div className="percy-min-width">
-  <Header
-    buttons={[
-      <Button>Dashboard</Button>,
-      <Button outline>File Repository</Button>]}
-  />
+    <Header
+      buttons={[
+        <Button>Dashboard</Button>,
+        <Button outline>File Repository</Button>]}
+    />
   </div>
 ));
